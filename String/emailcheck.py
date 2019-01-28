742ee367a721ae9162080698fd716cf398ac36d5
trushna4 <trushna4patel@gmail.com>
2019-01-27 22:01:58 -0500

    new

---
+++ String/emailcheck.py
@@ -0,0 +1,9 @@
+import re
+from pip._vendor.distlib.compat import raw_input
+
+import re
+
+email_regex = re.compile(r'^[a-z0-9_-]+@[a-z0-9]+\.[a-z]{1,3}$')
+emails = [raw_input()]
+for email in range(int(raw_input())):
+    print (sorted(filter(lambda email: re.match(email_regex, email), emails)))
